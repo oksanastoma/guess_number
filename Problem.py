d30130f6a699e96681784fc5fa9aaff75539a023
oksana <oksanastoma93@gmail.com>
2018-05-13 22:32:20 +0300

    Fibonacci numbers

---
+++ Problem.py
@@ -0,0 +1,54 @@
+Last login: Wed Apr 18 19:36:32 on ttys008
+MacBook-Pro-Dasha:~ dasha$ python3
+Python 3.6.5 (default, Mar 30 2018, 06:41:49) 
+[GCC 4.2.1 Compatible Apple LLVM 8.0.0 (clang-800.0.42.1)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 
+>>> (env) mbp-dasha:address-book-api dasha$ python
+  File "<stdin>", line 1
+    (env) mbp-dasha:address-book-api dasha$ python
+            ^
+SyntaxError: invalid syntax
+>>> python Problem1.py
+  File "<stdin>", line 1
+    python Problem1.py
+                  ^
+SyntaxError: invalid syntax
+>>>     
+... 
+>>> dasha$ python
+  File "<stdin>", line 1
+    dasha$ python
+         ^
+SyntaxError: invalid syntax
+>>> (env) mbp-dasha:address-book-api dasha$ python
+  File "<stdin>", line 1
+    (env) mbp-dasha:address-book-api dasha$ python
+            ^
+SyntaxError: invalid syntax
+>>> mkdir djangogirls
+  File "<stdin>", line 1
+    mkdir djangogirls
+                    ^
+SyntaxError: invalid syntax
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
